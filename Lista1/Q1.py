b5ec147eba6dbe2976688b6135961b005cb2acbf
LuizCarlosArt <sacramento.tecnico@gmail.com>
2021-04-13 11:45:54 -0300

    OK

---
+++ Lista1/Q1.py
@@ -0,0 +1,26 @@
+media = 0;
+deno =0
+a = float(input("Digite o valor de A: "))
+if a > 6:
+   media = a*5
+   deno = 5 
+else : 
+  media = a*2.5
+  deno = 2.5
+b = float(input("Digite o valor de B: "))
+if b > 6: 
+  media =+ b*5
+  deno =+ 5 
+else:
+   media =+ b*2.5
+   deno =+ 2.5
+c = float(input("Digite o valor de C "))
+if c > 6:
+  media =+ c*5
+  deno =+ 5 
+else:
+  media =+ c*2.5
+  deno =+ 2.5 
+print("A média é: ",(media/deno))
+  
+  
